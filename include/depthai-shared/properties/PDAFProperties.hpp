5ef420c021f4422f5efab384d5d8a3ce33a3cfbd
MarioSavu <mariosavu256@gmail.com>
2023-06-23 18:18:49 +0300

    Adding PDAF Kalman parameters to the PDAF property list

--- include/depthai-shared/properties/PDAFProperties.hpp
+++ include/depthai-shared/properties/PDAFProperties.hpp
@@ -14,9 +14,13 @@ struct PDAFProperties : PropertiesSerializable<Properties, PDAFProperties> {
     uint8_t refocus_reset_value;
     float pdaf_filter_threshold;
     uint8_t defaultFocusValue;
+    float kalmanZ;
+    float kalmanP;
+    float kalmanR;
+    float kalmanQ;
 };
 
 DEPTHAI_SERIALIZE_EXT(PDAFProperties, numFramesToSkip, refocus_bounds_min,
-    refocus_bounds_max, refocus_reset_value, pdaf_filter_threshold, defaultFocusValue);
+    refocus_bounds_max, refocus_reset_value, pdaf_filter_threshold, defaultFocusValue, kalmanZ, kalmanP, kalmanR, kalmanQ);
 
 }  // namespace dai
